6707eb78c369096a4257dfa7c3c087bff5448161
Nikshay <nikshay2018@gmail.com>
2023-07-01 17:03:20 +0530

    Added Develop Branch

--- new.cpp
+++ new.cpp
@@ -3,5 +3,7 @@ using namespace std;
 
 int main(){
     // This is just sample code file using which I am going to learn about various features of Git.
+    // Welcome to the develop branch of our project.
+    // In this branch all the changes that are supposed to test will be made and if they workout they will be merged with the master
     return 0;
 }
